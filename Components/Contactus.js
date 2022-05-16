abd98d58f610c6eae1073dd20f4c2809bcbbe62c
HorusCorp <arnaud.rey.job@gmail.com>
2022-05-16 16:56:05 +0200

    petite modif

--- Components/Contactus.js
+++ Components/Contactus.js
@@ -4,7 +4,7 @@ function Contactus() {
   return (
     <div className='contactus'>
         <div>
-            <p> Pour avoir plsu d'informations sur nos services contactez nous par téléphone au <strong>06 61 95 65 73</strong> ou par email à l'adresse suivante :</p>
+            <p> Pour avoir plus d'informations sur nos services contactez nous par téléphone au <strong>06 61 95 65 73</strong> ou par email à l'adresse suivante :</p>
             <p><strong>prasana-contact@gmail.com</strong></p>
         </div>
     </div>
